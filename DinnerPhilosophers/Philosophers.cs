171f29e6ec6ccaa5d71d1461759b1c963710dc7a
MrChemp <ruslansemenyak911@gmail.com>
2021-08-26 17:00:44 +0300

    add worked

---
+++ DinnerPhilosophers/Philosophers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace DinnerPhilosophers
+{
+    public class Philosophers
+    {
+        public string Name;
+        public Stick Left;
+        public Stick Right;
+        public int Ate = 0;
+        
+        public void Eatting(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                var takeLeftStick = Left.TakeStick();
+                var takeRightStick = Right.TakeStick();
+                if (takeLeftStick && takeRightStick)
+                {
+                    ++Ate;
+                    Console.WriteLine($"{Name} eat {Ate} time");
+                    //a highly respected philosopher needs time to eat
+                    Thread.Sleep(2000);
+                }
+                if (takeLeftStick) Left.PutStick();
+                if (takeRightStick) Right.PutStick();
+
+                //a highly respected philosopher needs time to think
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
---
+++ DinnerPhilosophers/Stick.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace DinnerPhilosophers
+{
+    public class Stick
+    {
+        private Mutex _mute = new Mutex(); 
+
+        public bool TakeStick()
+        {
+            if (_mute.WaitOne(3000)) return true;
+            else return false;
+        }
+
+        public void PutStick()
+        {
+            _mute.ReleaseMutex();
+        }
+    }
+}
